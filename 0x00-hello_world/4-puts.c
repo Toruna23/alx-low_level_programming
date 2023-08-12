30936406783459d2725e00e6cf032c6d9280a4be
root <mandilakhesomdle@gmail.com>
2023-08-12 01:30:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program thaat prints a line with puts function
+ * Return: 0 (success)
+ */
+
+int main (void)
+{
+	puts ("programming is like building a multilingual puzzle" "\n")
+	return (0);
+}
